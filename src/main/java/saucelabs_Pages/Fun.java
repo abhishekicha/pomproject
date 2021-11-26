39ac561a963f6b7d06dafb5fac90578ad0f07cc5
ABHISHEK PRAMANIK <apramanik28@gmail.com>
2021-11-26 12:56:31 +0530

    26-11-2021

---
+++ src/main/java/saucelabs_Pages/Fun.java
@@ -0,0 +1,35 @@
+package saucelabs_Pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Fun {
+
+	WebDriver driver;
+	public Fun(WebDriver kdriver) {
+		this.driver= kdriver;
+	}
+	
+	@FindBy(xpath="//div[contains(text(),'Get Started Guide')]//../../../../../a") 	
+	WebElement GetStarted;
+	@FindBy(xpath="//label[@for='manualTesting']//../../input") 	
+	WebElement radio;
+	@FindBy(xpath="//span[contains(text(),'Mobile app')]\r\n") 	
+	WebElement mobile;
+	@FindBy(xpath="//div[@class='RecentAndSavedConfigs__test-start__2mGhE']//button") 	
+	WebElement start;
+	
+	public void checkFunctions() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+		GetStarted.click();
+		radio.click();
+		mobile.click();
+		start.click();
+
+		//driver.quit();
+	}
+	
+}
---
+++ src/main/java/saucelabs_testcases/FuncionalityTest.java
@@ -0,0 +1,30 @@
+package saucelabs_testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import saucelabs_Pages.Fun;
+import saucelabs_Pages.LoginPage;
+
+public class FuncionalityTest {
+	@Test
+	public void checkFunctions() throws InterruptedException {
+	
+	//launch browser
+			WebDriverManager.chromedriver().setup();
+	        ChromeDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://saucelabs.com/");
+			
+			//created page object using page factory
+			LoginPage login_page = PageFactory.initElements(driver, LoginPage.class);
+			//call the method
+			login_page.login_to_saucedemo("abbypra", "Avi@1994");
+			Fun fun_page = PageFactory.initElements(driver, Fun.class);
+			fun_page.checkFunctions();
+
+	}
+}
