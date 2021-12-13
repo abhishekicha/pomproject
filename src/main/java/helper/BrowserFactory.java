702c1f5d246e39855442bfa639e78ecce0205ee0
ABHISHEK PRAMANIK <apramanik28@gmail.com>
2021-12-13 12:27:36 +0530

    13-12-2021(2)

--- src/main/java/helper/BrowserFactory.java
+++ src/main/java/helper/BrowserFactory.java
@@ -2,6 +2,7 @@ package helper;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -14,16 +15,20 @@ public class BrowserFactory {
 	public static  WebDriver startBrowser(String browserName, String url) {
 		
 		if(browserName.equalsIgnoreCase("firefox")) {
-			
+			WebDriverManager.firefoxdriver().setup();
 			driver= new FirefoxDriver();
 		}else if(browserName.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}
 		else if(browserName.equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
 			driver = new InternetExplorerDriver();
 		}
-		
+		else if(browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get(url);
 		
--- src/main/java/saucelabs_testcases/ZoneTest.java
+++ src/main/java/saucelabs_testcases/ZoneTest.java
@@ -1,12 +1,10 @@
 package saucelabs_testcases;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import helper.BrowserFactory;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import saucelabs_Pages.Zone;
 import saucelabs_Pages.LoginPage;
 
@@ -15,7 +13,7 @@ public class ZoneTest {
 	@Test 
 	public void checkSauce() throws InterruptedException {
 		//launch browser
-		WebDriver driver =	BrowserFactory.startBrowser("chrome", "https://saucelabs.com/");
+		WebDriver driver =	BrowserFactory.startBrowser("edge", "https://saucelabs.com/");
 
 		
 		//created page object using page factory
